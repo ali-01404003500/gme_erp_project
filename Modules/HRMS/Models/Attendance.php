0344443054566e5ba54b93b564090df37703f09a
ali-01404003500 <ali.01404003500@gmail.com>
2026-03-31 18:39:37 +0600

    factory error

--- Modules/HRMS/Models/Attendance.php
+++ Modules/HRMS/Models/Attendance.php
@@ -7,6 +7,7 @@ use App\Models\User;
 use App\Traits\AutoCreateUpdateAndHistory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Modules\HRMS\Database\Factories\AttendanceFactory;
 use Modules\HRMS\Models\Settings\Shift;
 
 class Attendance extends BaseModel
@@ -21,8 +22,8 @@ class Attendance extends BaseModel
      * @return \Illuminate\database\Eloquent\Factories\Factory
      */
     protected static function newFactory()
-    {
-        return \Modules\HRMS\database\Factories\AttendanceFactory::new();
+    { 
+        return AttendanceFactory::new();
     }
 
 
