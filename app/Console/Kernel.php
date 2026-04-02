d18e025b05ab5fa2f8999332f4e62b478d006de1
ali-01404003500 <ali.01404003500@gmail.com>
2026-04-02 19:11:34 +0600

    sms er kaj koreci

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,6 +14,7 @@ class Kernel extends ConsoleKernel
     {
         $schedule->command('attendance:auto-create')->dailyAt('00:10')->timezone('Asia/Dhaka')->withoutOverlapping();
         $schedule->command('bills:generate')->dailyAt('00:11')->timezone('Asia/Dhaka')->withoutOverlapping();
+        $schedule->command('sms:send-pending')->everyMinute()->timezone('Asia/Dhaka')->withoutOverlapping();
         
     }
     /**
@@ -25,4 +26,6 @@ class Kernel extends ConsoleKernel
 
         require base_path('routes/console.php');
     }
+
+    
 }
