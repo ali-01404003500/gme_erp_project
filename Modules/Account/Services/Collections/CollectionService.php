c9d1ae0cddb7086388cb349be843a2fff4a91bd4
ali-01404003500 <ali.01404003500@gmail.com>
2026-04-07 14:02:18 +0600

    collection service update

--- Modules/Account/Services/Collections/CollectionService.php
+++ Modules/Account/Services/Collections/CollectionService.php
@@ -263,7 +263,7 @@ class CollectionService
                             'cheque_no' => $payment->transaction_id,
                             'cheque_date' => $payment->date,
                             'amount' => $payment->amount,
-                            "document" => $payment->attachments,
+                            'document' => is_array($payment->attachments) ? json_encode($payment->attachments)  : json_encode([$payment->attachments]),
                             "remarks" => $payment->remarks,
                         ]);
                         $payment->load('chequeVerification');
@@ -287,7 +287,7 @@ class CollectionService
                             'head_id' => $payment->bank_id,  
                             'deposit_date' => $payment->date,
                             'amount' => $payment->amount,
-                            "document" => $payment->attachments,
+                            'document' => is_array($payment->attachments) ? json_encode($payment->attachments)  : json_encode([$payment->attachments]),
                             "remarks" => $payment->remarks,
                         ]);
                         $payment->load('onlineDepositVerification');
