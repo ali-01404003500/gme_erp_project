f3d6d2bfae8ee382b51567df8529bb9ca4460f50
ali-01404003500 <ali.01404003500@gmail.com>
2026-04-07 17:21:48 +0600

    ok

--- Modules/Account/Services/OnlineDepositVerificationService.php
+++ Modules/Account/Services/OnlineDepositVerificationService.php
@@ -102,7 +102,7 @@ class OnlineDepositVerificationService
     {
         /**
          */
-        // dd($onlineDepositVerification->payments);
+         dd($onlineDepositVerification->payments);
         $onlineDepositVerification->transactions()->delete();
 
          
