5eb8586dcd8b6e89873e94447b85ea2389c93053
GME-BD <gmebdltd@gmail.com>
2026-04-26 15:43:34 +0600

    added search bar in Stock_Report

--- Modules/Inventory/resources/views/stocks/stock-in-hand.blade.php
+++ Modules/Inventory/resources/views/stocks/stock-in-hand.blade.php
@@ -2,7 +2,6 @@
 @section('title', "Stock Report")
 @section('description', "Stock Report for Stock in hand")
 @section('content')
-    <!-- CONTENT AREA -->
     <div class="container-fluid">
         <div class="social-dash-wrap">
             <div class="row">
@@ -20,12 +19,6 @@
                         </div>
                         <div class="breadcrumb-main__wrapper">
                             <div class="action-btn mt-sm-0 mt-15">
-                                {{-- @if (hasPermission('inv.settings.units.create'))
-                                <button class="btn btn-xs btn-primary me-1" data-bs-toggle="modal"
-                                    data-bs-target="#createModal">
-                                    Add New
-                                </button>
-                                @endif --}}
                                 <a href="{{ request()->fullUrlWithQuery(['export' => 'pdf']) }}" target="_blank"
                                     class="btn btn-danger btn-sm d-inline-block mr-2">
                                     <i class="las la-file-pdf fs-16"></i> PDF
@@ -47,13 +40,22 @@
 
             <div class="row">
                 <div class="col-md-12" style="padding-bottom: 20px">
-                    <div class="row" style="width: 100%">
+                    <div class="row align-items-center" style="width: 100%">
                         <div class="col-md-6">
-                            <h4 class="text-capitalize breadcrumb-title">{{ trans('menu.stock-menu-title') }}
-                            </h4>
+                            <h4 class="text-capitalize breadcrumb-title">{{ trans('menu.stock-menu-title') }}</h4>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="input-group" style="max-width: 300px; float: right;">
+                                <span class="input-group-text bg-white border-end-0">
+                                    <i class="las la-search"></i>
+                                </span>
+                                <input type="text" id="tableSearch" class="form-control border-start-0"
+                                    placeholder="Search Product Name...">
+                            </div>
                         </div>
                     </div>
                 </div>
+
                 <div class="col-md-12">
                     <div class="card mb-4">
                         <div class="card-body">
@@ -93,7 +95,6 @@
                                 <thead>
                                     <tr>
                                         <th class="text-center" style="width: 8%">Sl</th>
-
                                         <th>Product Name</th>
                                         <th>In</th>
                                         <th>Out</th>
@@ -101,7 +102,6 @@
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    {{-- @dd($stocks) --}}
                                     @foreach ($stocks as $key => $stock)
                                         <tr>
                                             <td class="text-center">{{ $key + 1 }}</td>
@@ -124,12 +124,10 @@
         </div>
     </div>
 
-    <!-- EDIT MODAL -->
     <div class="modal fade inputForm-modal" id="product-ledger-modal" tabindex="-1" role="dialog"
         aria-labelledby="product-ledger-modal" aria-hidden="true">
         <div class="modal-dialog modal-xl" role="document">
             <div class="modal-content">
-
                 <div class="modal-header" id="editModalLabel">
                     <h5 class="modal-title">Product Ledger </h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-hidden="true"></button>
@@ -147,10 +145,30 @@
 @endsection
 
 @section('page_scripts')
-
     <script>
         $(document).ready(function () {
+            // Initialize DataTable
+            var table = $('#stock-table').DataTable({
+                "dom": 'lrtip',
+                "paging": true,
+                "info": true,
+                "lengthChange": true,
+                "pageLength": 20, 
+                "lengthMenu": [[10, 25, 50, 100, -1], [10, 25, 50, 100, "All"]], 
+                "language": {
+                    "paginate": {
+                        "previous": "<i class='las la-angle-left'></i>",
+                        "next": "<i class='las la-angle-right'></i>"
+                    }
+                }
+            });
 
+            // Custom Search for Product Name (Column Index 1)
+            $('#tableSearch').on('keyup', function () {
+                table.columns(1).search(this.value).draw();
+            });
+
+            // Modal loading logic
             $(document).on('click', '.show-product', function () {
                 var ledgerUrl = $(this).data('url');
                 $('#product-ledger-modal').find('.modal-body').loadWithSpinner(ledgerUrl);
