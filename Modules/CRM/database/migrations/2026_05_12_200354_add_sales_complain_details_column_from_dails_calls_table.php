42373384278f4ad0c6a9c2e1bf48d27686ed08b4
ali-01404003500 <ali.01404003500@gmail.com>
2026-05-12 20:04:55 +0600

    add sales_complain_details column in  dails_calls tbale

---
+++ Modules/CRM/database/migrations/2026_05_12_200354_add_sales_complain_details_column_from_dails_calls_table.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('daily_calls', function (Blueprint $table) {
+            $table->text('sales_complain_details')
+                  ->nullable()
+                  ->after('product_required_details');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('daily_calls', function (Blueprint $table) {
+            $table->dropColumn('sales_complain_details');
+        });
+    }
+};
